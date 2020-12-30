88d6d44c7b375a78f7829b065830274ac01a4ae9
starq <greyhands2@gmail.com>
2020-12-30 22:54:15 +0100

    adding all blog code for gatsby set up

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,54 @@
  */
 
 // You can delete this file if you're not using it
+
+//note code here is gatsby node api based
+//note gatsby likes to use back ticks `` instead of single or double quotes '' or ""
+
+const path = require('path')
+const {createFilePath} = require(`gatsby-source-filesystem`)
+
+
+exports.onCreateNode = ({node, getNode, actions}) =>{
+    const {createNodeField} = actions
+    if(node.internal.type===`MarkdownRemark`){
+        const slug = createFilePath({node, getNode})
+        // adding a new field called slug into our graphql query
+        createNodeField({
+            node,
+            name: `slug`,
+            value: slug
+        })
+    }
+}
+
+
+exports.createPages= ({graphql, actions}) => {
+    const {createPage} = actions
+    //in the frontend we get to use es6 graphql`` but here we do not get direct access to es6 so we use graphql(``)
+    return graphql(`
+        {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+
+    `).then(result=>{
+        result.data.allMarkdownRemark.edges.forEach(({node})=>{
+            createPage({
+                path: node.fields.slug,
+                component: path.resolve(`./src/templates/blog-post.js`),
+                context:{
+                    slug: node.fields.slug
+                }
+            })
+        })
+    })
+}
+
---
+++ src/pages/page-3.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+import Layout from "../components/layout"
+ 
+
+const ThirdPage = () => (
+  <Layout>
+    
+    <h1>Hi from the third page</h1>
+    
+  </Layout>
+)
+
+export default ThirdPage
