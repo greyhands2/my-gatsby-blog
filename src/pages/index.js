df05aa250890e1343982c42c49963ff93919643c
tonyStarq <starq@pop-os.localdomain>
2021-04-26 23:13:04 +0100

    new change

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,7 +57,7 @@ export const query = graphql`
             slug
           }
           html
-          excerpt(truncate: true)
+          excerpt(truncate: false)
         }
       }
       totalCount
